ff79d4443f9c080953e2c6550232d84b67545028
Ключникова Валерия <vklyuchnikova@atollis.com>
2026-05-13 21:42:33 +0300

    wishlists page loading

--- app/(main)/wishlists/page.tsx
+++ app/(main)/wishlists/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useUser from "@/api/useUser";
 import useUserWishlists from "@/api/useUserWishlists";
+import Skeleton from '@/components/Skeleton';
 import { Typography } from "@/components/Typography";
 import EmptyWishlists from "@/emptystates/EmptyWishlists";
 import { cn } from "@/shared/cn";
@@ -11,12 +12,6 @@ import WishlistCreate from "@/widgets/Wishlist/WishlistCreate";
 const WishlistsPage = () => {
   const { data: wishlists = [], isLoading } = useUserWishlists(true);
   const { data: user } = useUser(true);
-  // const wishlists = await getUserWishlists();
-  // const user = await getServerUserPayload();
-
-  if (isLoading) {
-    return <>Loading...</>;
-  }
 
   if (!wishlists.length && !isLoading) {
     return <EmptyWishlists />;
@@ -27,13 +22,14 @@ const WishlistsPage = () => {
       {!!user && !user.is_verified && <EmailVerificationBanner className="mb-5" />}
       <div className="flex flex-col gap-4 sm:flex-row sm:justify-between sm:items-center">
         <Typography.H2>Мои Вишлисты</Typography.H2>
-        {((wishlists.length < 3 && !!wishlists.length) || user?.role === "admin") && <WishlistCreate disabled={!user?.is_verified} />}
+        {((wishlists.length < 3 && !!wishlists.length) || user?.role === "admin") && <WishlistCreate disabled={!user?.is_verified || isLoading} />}
       </div>
       <div
         className={cn("mt-5 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 auto-rows-fr", {
-          "grid-cols-[repeat(auto-fit,minmax(500px,1fr))]!": wishlists.length > 2,
+          "grid-cols-[repeat(auto-fit,minmax(500px,1fr))]!": wishlists.length > 2 || isLoading,
         })}
       >
+        {isLoading && <Skeleton asChild count={3} className='h-64' />}
         {wishlists.map((wishlist) =>
           user?.is_verified ? (
             <WishlistCard key={wishlist.id} {...wishlist} userName={user?.name} />
